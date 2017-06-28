435c509206ca8df53dbd49cb6bef44dd2053762e
Carlton Schuyler <cschuyler@rallydev.com>
2017-06-27 18:36:41 -0600

    Cleaning up HttpClient reources

--- src/main/java/dragnon/doopercrawl/Main.java
+++ src/main/java/dragnon/doopercrawl/Main.java
@@ -6,11 +6,11 @@ import static java.util.Comparator.comparing;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            if (args.length != 1) {
-                throw new RuntimeException("");
-            }
-            new Browser(new PageProcessor(new LinkExtractor(args[0], new LinkNormalizer())), new FollowPolicy(args[0]))
+        if (args.length != 1) {
+            throw new RuntimeException("");
+        }
+        try (PageProcessor pageProcessor = new PageProcessor(new LinkExtractor(args[0], new LinkNormalizer()))) {
+            new Browser(pageProcessor, new FollowPolicy(args[0]))
                     .crawl(args[0])
                     .getLinks().stream()
                     .sorted(comparing(Link::from)
--- src/main/java/dragnon/doopercrawl/PageProcessor.java
+++ src/main/java/dragnon/doopercrawl/PageProcessor.java
@@ -1,34 +1,38 @@
 package dragnon.doopercrawl;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static com.google.common.net.HttpHeaders.USER_AGENT;
-
-class PageProcessor implements Function<String, Stream<String>> {
+class PageProcessor implements Function<String, Stream<String>>, Closeable {
     //            LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
     //            int cores = Runtime.getRuntime().availableProcessors();
     //            ThreadPoolExecutor executor = new ThreadPoolExecutor(cores, 2 * cores, 1, TimeUnit.MINUTES, queue);
 
+    private CloseableHttpClient client = null;
     private Function<String, Stream<String>> linkExtractor;
 
     public PageProcessor(Function<String, Stream<String>> linkExtractor) {
         this.linkExtractor = linkExtractor;
+        initHttpClient();
     }
 
     @Override
     public Stream<String> apply(String url) {
 //        System.out.println("PAGE: "+url);
         System.out.print(".");
-        StringBuffer result = null;
+        StringBuffer result;
         try {
             result = getContent(url);
         } catch (IOException e) {
@@ -37,22 +41,56 @@ class PageProcessor implements Function<String, Stream<String>> {
         return linkExtractor.apply(result.toString());
     }
 
+    // https://hc.apache.org/httpcomponents-client-ga/tutorial/html/connmgmt.html
+    private void initHttpClient() {
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        // Increase max total connection to 200
+        cm.setMaxTotal(200);
+        // Increase default max connection per route to 20
+        cm.setDefaultMaxPerRoute(20);
+
+        client = HttpClients.custom()
+                .setConnectionManager(cm)
+                .build();
+    }
+
+    // https://www.mkyong.com/java/apache-httpclient-examples/
     private StringBuffer getContent(String url) throws IOException {
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
+        CloseableHttpResponse response = null;
+        HttpGet request = null;
+        HttpEntity entity = null;
+        try {
+            request = new HttpGet(url);
 
-        // add request header
-        request.addHeader("User-Agent", USER_AGENT);
-        HttpResponse response = client.execute(request);
+            // add request header
+            request.addHeader("User-Agent", "Mozilla 5.0");
+            response = client.execute(request);
 
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
+            entity = response.getEntity();
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(entity.getContent()));
 
-        StringBuffer result = new StringBuffer();
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+            StringBuffer result = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            return result;
+        } finally {
+            if (entity != null) {
+                EntityUtils.consume(entity);
+            }
+            if (response != null) {
+                response.close();
+            }
+            if (request != null) {
+                request.releaseConnection();
+            }
         }
-        return result;
+    }
+
+    @Override
+    public void close() throws IOException {
+        client.close();
     }
 }
